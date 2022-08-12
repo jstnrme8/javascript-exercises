177731acef235e683ade42c0b7734ff677db9fc9
jstnrme8 <jr.ardepuela@gmail.com>
2022-08-12 13:18:17 +0800

    Done js 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        for (let j = theArgs.length-1; j >=0; j--) {
+            if (arr[i] === theArgs[j]) {
+                arr.splice(i, 1);
+            }
+        }
+    }
+
+    return arr;
 
 };
 
