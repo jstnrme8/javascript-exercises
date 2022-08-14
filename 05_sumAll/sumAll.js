dac4b4b1d044c20e768db0d6c4c83f4244490824
jstnrme8 <jr.ardepuela@gmail.com>
2022-08-14 23:25:37 +0800

    Done js 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+
+    let max = args[1];
+    let min = args[0];
+
+    let total = 0;
+
+    if (max < 0 || min < 0 || typeof max == "string" || typeof min == "string" || max.constructor == Array || min.constructor == Array) return "ERROR";
+    else if (max > min){
+        for (let i = min; i <= max; i += min){
+            total += i;
+        } 
+    } else {
+        for (let i = max; i <= min; i += max){
+            total += i;
+        } 
+    }
+    
+    return total;
+
+    
 
 };
 
