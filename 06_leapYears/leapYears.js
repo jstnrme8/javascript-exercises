69472e6aa70e8ffa2f54d13e5c8174f917371170
jstnrme8 <jr.ardepuela@gmail.com>
2022-08-14 23:41:55 +0800

    Done js 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,8 @@
-const leapYears = function() {
+const leapYears = function(leap) {
+
+    if (leap % 100 === 0 && leap % 400 != 0) return false;
+    else if (leap % 4 != 0) return false;
+    else return true;
 
 };
 
