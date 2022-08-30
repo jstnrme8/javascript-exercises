c0fcf4ab32d86f3c6c6b1702e0208277904d0384
jstnrme8 <jr.ardepuela@gmail.com>
2022-08-30 21:06:28 +0800

    Done 08 js exercises calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const someArray = array;
+	let total = 0;
+
+  for ( let i = 0; i < someArray.length; i++) {
+    total += someArray[i];
+  }
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  const someArray = array;  
+  let product = 1;
+
+  for ( let i = 0; i < someArray.length; i++) {
+    product *= someArray[i];
+  }
+
+  return product;
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let rval = 1;
+  for (let i = 2; i <= a; i++) {
+    rval *= i;
+  }
+
+  return rval;
 };
 
 // Do not edit below this line
