e8dfbb11a1ce0093f8ce64ccc312d9e0e44fc5f1
jstnrme8 <jr.ardepuela@gmail.com>
2022-08-30 22:22:28 +0800

    Done 10 js exercises fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if (a < 0) return 'OOPS';
+    let n1 = 0, n2 = 1, nextTerm;
+
+    for (let i = 1; i <= a; i++) {
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+
+    return n1;
+
+    //0 + 1 = 1
+    //1==4 flag=2
+    //1 + 2 = 3
+    //flag = 3;
 
 };
 
