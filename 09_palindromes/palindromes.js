acfaefd9c2a0968f0513aaf45298919f79a35f83
jstnrme8 <jr.ardepuela@gmail.com>
2022-08-30 22:00:01 +0800

    Done 09 js exercises palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    let reg = /[^A-Za-z0-9]/g;
+    let toLower = str.toLowerCase().replace(reg, '');
+    let reversed = toLower.split('').reverse().join('');
+
+    return toLower === reversed;
 };
 
 // Do not edit below this line
